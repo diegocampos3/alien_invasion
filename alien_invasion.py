c9ff029aad1527f2fdb34aac286d0b246fa38f98
Diego Campos <camposdiegog3@gmail.com>
2024-08-21 09:44:55 -0500

    Tasa de frames, color de fondo, clase Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,13 +2,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Clase general para gestionar los recursos y el comportamiento del juego"""
 
     def __init__(self):
         """Inializa el juego y crea recursos"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
     
     def run_game(selft):
@@ -18,9 +23,12 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                selft.screen.fill(selft.settings.bg_color)
         
-        #Hace visible la última pantalla dibujada
-        pygame.display.flip() 
+            #Hace visible la última pantalla dibujada
+            pygame.display.flip() 
+            selft.clock.tick(60)
+
 
 if __name__ == '__main__':
     #Hace una instancia del juego y lo ejecuta
